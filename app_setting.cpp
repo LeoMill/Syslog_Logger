0cf64da9e3fdd5dc592163d5b66265347cb0f10f
Bruce <bruce@yare.com.tw>
2020-05-14 12:55:24 +0800

    Add icon and tab
    Add icon files and tab in setting

--- app_setting.cpp
+++ app_setting.cpp
@@ -1,13 +1,20 @@
 #include <QTabWidget>
+#include <QGridLayout>
 
 #include "app_setting.h"
 
 App_Setting::App_Setting()
 {
-    setWindowTitle("Setup");
     QTabWidget *tab_widget = new QTabWidget;
-    tab_widget->addTab(new QWidget(),tr("Tab 1"));
+    tab_widget->addTab(new QWidget(),tr("Main"));
+    tab_widget->addTab(new QWidget(),tr("Files"));
+    tab_widget->addTab(new QWidget(),tr("E-mail"));
 
+    QGridLayout *mainLayout = new QGridLayout;
+    mainLayout->addWidget(tab_widget, 0, 0);
+    setLayout(mainLayout);
+    setWindowTitle("Setup");
+    setWindowFlags(windowFlags() & ~Qt::WindowContextHelpButtonHint);
 }
 
 App_Setting::~App_Setting()
